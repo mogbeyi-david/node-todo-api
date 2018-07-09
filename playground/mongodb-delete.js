41d887a34a9888d2b4614e3e18a7bf9488b81a19
David Mogbeyi <mogbeyidavid@gmail.com>
2018-07-09 09:54:13 +0100

    Code Refactor 01

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,19 +6,4 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB Server');
-  //deleteMany
-  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
-  //   console.log(result);
-  // });
-
-  //deleteOne
-  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
-  //   console.log(result);
-  // });
-
-  //findOneAndDelete
-  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-  //   console.log(result);
-  // });
-  //db.close();
 });
